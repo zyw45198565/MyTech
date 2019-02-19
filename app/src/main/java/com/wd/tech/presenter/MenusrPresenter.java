4d6d0f7a774253502a12f2afe7951534fec31961
why-x <1564760869@qq.com>
2019-02-19 19:44:24 +0800

    44

---
+++ app/src/main/java/com/wd/tech/presenter/MenusrPresenter.java
@@ -0,0 +1,24 @@
+package com.wd.tech.presenter;
+
+import com.wd.tech.core.Interfacea;
+import com.wd.tech.utils.DataCall;
+import com.wd.tech.utils.NetWorkManager;
+
+import io.reactivex.Observable;
+
+/*
+banner展示列表
+ */
+public class MenusrPresenter extends WDPresenter{
+    public MenusrPresenter(DataCall dataCall) {
+        super(dataCall);
+    }
+
+    @Override
+    protected Observable observable(Object... args) {
+        Interfacea interfacea = NetWorkManager.getInstance().create(Interfacea.class);
+        return interfacea.mymenus();
+    }
+
+
+}
