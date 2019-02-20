9d8820cd28bbd57e178f3037b050ba34ff30e61e
zyw45198565 <863685516@qq.com>
2019-02-20 14:16:41 +0800

    213321

---
+++ app/src/main/java/com/wd/tech/activity/MySettingActivity.java
@@ -0,0 +1,29 @@
+package com.wd.tech.activity;
+
+import com.wd.tech.R;
+import com.wd.tech.utils.util.WDActivity;
+
+/**
+ * @author Tech
+ * @date 2019/2/20 11:10
+ * QQ:45198565
+ * 佛曰：永无BUG 盘他！
+ */
+public class MySettingActivity extends WDActivity {
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_mysettings;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void destoryData() {
+
+    }
+}
